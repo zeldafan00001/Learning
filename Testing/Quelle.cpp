c36d786e66d9eda7027c492a3f5a6b40f49dbd88
Kevin <zeldafan00001@hotmail.de>
2019-03-27 16:19:37 +0100

    Update - Enemy Move
    Update - Enemy Move

--- Testing/Quelle.cpp
+++ Testing/Quelle.cpp
@@ -7,37 +7,88 @@
 using namespace std;
 	
 	//  [y][x]
-char map[4][4] = {	"###",
-					"###",
-					"###" };
+char map[10][10] = {"######",
+					"#X   #",
+					"#    #",
+					"#   X#",
+					"######" };
 
 int cordY[4], tempY;
 int cordX[4], tempX;
+int xe2, ye2;
+int s;
+int direction[4]; 
+// 1 UP // 2 DOWN
 
-int main()
+bool running = false;
+
+void setEnemCords()
 {
-	cordY[0] = 0; cordX[0] = 0;
-	cordY[1] = 1; cordX[1] = 1;
-	cordY[2] = 2; cordX[2] = 2;
+	cordY[0] = 1; cordX[0] = 1;	
+	cordY[1] = 3; cordX[1] = 4;
 
-	for (int d = 0; d < 4; d++)
+	direction[0] = 2;
+	direction[1] = 1;
+
+}
+void drawMap()
+{
+	for (int d = 0; d < 5; d++)
 	{
 		printf("%s\n", map[d]);
 	}
-	for (int s = 0; s < 4; s++)
+}
+void enemMove()
+{
+	for (s = 0; s < 2; s++)
 	{
+		tempY = cordY[s];	tempX = cordX[s];
 
-		tempY = cordY[s]; tempX = cordX[s];
-		map[tempY][tempX] = 'X';
-		printf("Y%d  X%d\n", tempY, tempX);
-		Sleep(1500);
-		for (int d = 0; d < 4; d++)
+		if (direction[s] == 1) // Hoch laufen
 		{
-			printf("%s\n", map[d]);
-		}
-		printf("\n\n");
+			if (map[tempY - 1][tempX] != '#')
+			{
+				map[tempY][tempX] = ' ';
+				tempY--;
+				map[tempY][tempX] = 'X';
+				cordY[s] = tempY;
+				cordX[s] = tempX;
+			}
+			else //(map[tempY - 1][tempX] == '#') 
+			{
+				direction[s] = 2;
+			}
+		} // Hoch bewegen!
+
+		if (direction[s] == 2) // Runter laufen
+		{
+			if (map[tempY + 1][tempX] != '#')
+			{
+				map[tempY][tempX] = ' ';
+				tempY++;
+				map[tempY][tempX] = 'X';
+				cordY[s] = tempY;
+				cordX[s] = tempX;
+			}
+			else //(map[tempY + 1][tempX] == '#')
+			{
+				direction[s] = 1;
+			}
+		} // Runter Bewegen
+
+	}
+}
+int main()
+{
+	setEnemCords();
+	while (!running)
+	{
+		system("cls");
+		drawMap();
+		enemMove();
+		Sleep(500);
 	}
 
-	getchar();
+	system("cls");
 	return 0;
 }
