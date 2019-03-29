75149acda9c653e4267bc14e78a191b4dea044ae
Kevin <zeldafan00001@hotmail.de>
2019-03-29 19:07:27 +0100

    Struct Testing v1
    Struct Testing v1

--- Struct Testing/Quelle.cpp
+++ Struct Testing/Quelle.cpp
@@ -14,32 +14,32 @@ struct Bot
 	int cordsX[10];
 };
 
+void structuren(Spieler Player, Spieler Gegner)
+{
+	for (int s = 0; s < 9; s++)
+	{
+		Player.cordsY[s] = s + 2;
+		Player.cordsX[s] = s + 5;
+		Gegner.cordsX[s] = s + 7;
+		Gegner.cordsY[s] = s + 3;
+	}
+}
+
 int main()
 {
-	Spieler Player;
-	Bot Gegner;
 
-		for (int s = 0; s < 9; s++)
-		{
-			Player.cordsY[s] = s + 2;
-			Player.cordsX[s] = s + 5;
-			Gegner.cordsX[s] = s + 7;
-			Gegner.cordsY[s] = s + 3;
-		}
-		cout << endl << "Player.CordsY" << endl;
-		for (int s = 0; s < 9; s++)
-		{
-			
-			cout << "X: " << Player.cordsX[s] << " | Y: " << Player.cordsY[s] << endl;
-		}
+	cout << endl << "Player.CordsY" << endl;
+	for (int s = 0; s < 9; s++)
+	{
+		cout << "X: " << Player.cordsX[s] << " | Y: " << Player.cordsY[s] << endl;
+	}
 
-		cout <<endl <<"Gegner.CordsY" << endl;
-		for (int s = 0; s < 9; s++)
-		{
+	cout << endl << "Gegner.CordsY" << endl;
+	for (int s = 0; s < 9; s++)
+	{
+		cout << "X: " << Gegner.cordsX[s] << " | Y: " << Gegner.cordsY[s] << endl;
+	}
 
-			cout << "X: " << Gegner.cordsX[s] << " | Y: " << Gegner.cordsY[s] << endl;
-		}
-
-		getchar();
+	getchar();
 	return 0;
 }
